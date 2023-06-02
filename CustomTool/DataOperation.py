0c4a0bd3500d4d98c073199861bd6a4bf5e28bd3
DreamWalkerJK <lunatic_jk@qq.com>
2023-06-02 10:24:59 +0800

    2023/6/2 列表分组bug修改

--- CustomTool/DataOperation.py
+++ CustomTool/DataOperation.py
@@ -1,3 +1,5 @@
+import math
+
 # 对列表进行分组
 def divideDataList(dataList, groupNum):
     if groupNum <= 1:
@@ -5,14 +7,17 @@ def divideDataList(dataList, groupNum):
     index = 0
     dataDivide = []
     divide = len(dataList) // groupNum
+    ceilDivide = math.ceil(len(dataList) / groupNum)
     remainder = len(dataList) % groupNum
+    flag = 0 if remainder == 0 else 1
     for i in range(1, groupNum + 1): # 对数据进行分组
         start = index
-        if(index == 0):
-            end = index + i * divide + remainder
+        if(flag <= remainder):
+            end = index + ceilDivide
         else:
             end = index + divide
         index = end
         data = [item for item in dataList[start: end]]
         dataDivide.append(data)
+        flag += 1
     return dataDivide
